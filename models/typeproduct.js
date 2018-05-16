4a42651521f2e4bba5cdf64a482435ebaab251a8
Dai Nguyen <38663324+thanhdai261097@users.noreply.github.com>
2018-05-16 14:33:02 +0700

    Dai #16/5 2:32pm add typeproduct.js

---
+++ models/typeproduct.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var MongoClient = require('mongodb').MongoClient; // connect online
+var uri = "mongodb+srv://duy:vippergod12@data-imllf.mongodb.net/test"; // connect online
+
+
+var someschema = new Schema({
+type: String,
+name: String
+});
+
+ function typeproductCollection(callback) {
+	MongoClient.connect(uri, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("3dwebsite");
+		dbo.collection("typeproduct").find().toArray(function(err, result) {
+			if (err) {
+				throw err;
+				console.log(err);
+			} else if (result.length > 0) {
+				callback(result);
+			}
+		});
+	});
+}
+
+
+
+module.exports = mongoose.model('typeproduct', someschema);
+module.exports.typeproductCollection = typeproductCollection;
