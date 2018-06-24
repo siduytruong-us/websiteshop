aa6ca1d4db87ab19079e1bdc922c275301232c04
Cris <duytruong0702@gmail.com>
2018-06-24 16:58:41 +0700

    Duy

--- demo.js
+++ demo.js
@@ -93,29 +93,29 @@ app.use(thongKe);
 app.use(productManager);
 // app.use(Language);
 
-// // Error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(req, res, next) {
-//     res.locals.session = req.session;
-//     next();
-// });
-//
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('errorPage', {
-//     error : err.message
-//   });
-// });
-//Dat
+// Error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('errorPage', {
+    error : err.message
+  });
+});
+
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
